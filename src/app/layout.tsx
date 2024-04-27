ce5968d3c50080182fe5bba357a3eb1faf25bf2b
iam-sphinx <kandarp1803@gmail.com>
2024-04-27 19:53:48 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/ThemeProivider";
 
 const inter = Inter({ subsets: ["latin"] });
 
